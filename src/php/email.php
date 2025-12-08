77b2fd7d5bae0c3b4a239235037e38cb5765ae39
Ced <cedrickced820@gmail.com>
2025-12-09 01:12:35 +0800

    Add: emailer using SMTP vendor, Works for subscribing purposes.

---
+++ src/php/email.php
@@ -0,0 +1,54 @@
+<?php
+  require "../../vendor/autoload.php"; 
+
+  if ($_SERVER["REQUEST_METHOD"] !== "POST") {
+    echo "Error: Please submit the form properly.";
+    exit;
+  }
+
+  
+  if (!isset($_POST["email"]) || empty($_POST["email"])) {
+    echo "Error: Email address is required.";
+    exit;
+  }
+
+  $email = $_POST["email"]; 
+
+  
+  if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    echo "Error: Invalid email format.";
+    exit;
+  }
+
+  use PHPMailer\PHPMailer\PHPMailer; 
+  use PHPMailer\PHPMailer\SMTP; 
+  use PHPMailer\PHPMailer\Exception;
+
+  $mail = new PHPMailer(true); 
+
+  try {
+    $mail->isSMTP(); 
+    $mail->SMTPAuth = true; 
+
+    
+    $mail->Host = "smtp.gmail.com"; 
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS; 
+    $mail->Port = 587; 
+    
+    
+    $mail->Username = "cedrickced820@gmail.com";
+    $mail->Password = "zbdideuwwygoxwnd"; 
+
+    $mail->setFrom("cedrickced820@gmail.com", "The Philippine Artisan");
+    $mail->addAddress($email);
+
+    $mail->Subject = "Welcome to The Philippine Artisan"; 
+    $mail->Body = "Thank you for subscribing with email: " . $email; 
+
+    $mail->send(); 
+    
+    echo "NASEND NA!!!"; 
+  } catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+  }
+?>
